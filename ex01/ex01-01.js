2d2ee6172ea2b95342a01ae8e3c57f0e5f844144
dlwlgns1214 <dlwlgns1214@naver.com>
2023-10-18 12:34:57 +0900

    1일차(2023.10.18)

---
+++ ex01/ex01-01.js
@@ -0,0 +1,13 @@
+var 사과 = 2500; //let, const, 선언
+var 바나나 = 4000;
+var 배 = 3000;
+var 키위 = 900;
+var 총합 = 사과 + 바나나 + 배 + 키위;
+console.log(사과 + 바나나 + 배);
+console.log(총합 - 키위);
+console.log(사과 + 바나나 + 배 + 키위);
+console.log(20000 - (사과 + 바나나 + 배 + 키위));
+console.log(20000 - 총합);
+var aa = "안녕";
+var bb = "하세요";
+console.log(aa + bb);
